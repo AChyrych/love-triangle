47d2faceea93a1a1c3b385d02d6ac22488a77f3e
AChyrych <a.chyrych@gmail.com>
2018-09-16 19:40:08 +0300

    love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+
+	var countLoverTriangles = 0;
+	var otherSpichonees = [];
+
+		for (var i = 0; i < preferences.length; i++) {
+			if (i!== preferences[i]-1){
+			    var aSpichonee = preferences[i]-1;
+	 		    var bSpichonee = preferences[aSpichonee]-1;
+	 		    var cSpichonee = preferences [bSpichonee]-1;
+
+	    		if (cSpichonee == i && otherSpichonees.includes(cSpichonee) === false){  
+	    		 
+	    		otherSpichonees.push(aSpichonee);
+	     		otherSpichonees.push(bSpichonee);
+	    		countLoverTriangles++; 
+	   		 }
+		}
+	}
+    return countLoverTriangles
   // your implementation
 };
